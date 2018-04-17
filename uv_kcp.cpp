04969faa90271ea4d1b04880a8acb5029f9f1eae
chuqq <chuqingq@qq.com>
2018-04-17 15:17:05 +0800

    性能测试

--- uv_kcp.cpp
+++ uv_kcp.cpp
@@ -1,16 +1,30 @@
 // libuv+kcp
-// g++ uv_kcp.cpp -g -std=c++11 -pthread -I../libuv/libuv_bin/include -L../libuv/libuv_bin/lib -luv
-// g++ uv_kcp.cpp -g -std=c++11 -Wall -pthread  -luv
+// g++ -o uv_kcp{,.cpp} -g -O3 -std=c++11 -Wall -pthread -I/home/chuqq/temp/libuv/libuv_bin/include -L/home/chuqq/temp/libuv/libuv_bin/lib -luv
+// g++ -o uv_kcp{,.cpp} -g -O3 -std=c++11 -Wall -pthread -luv
 // export LD_LIBRARY_PATH=../libuv/libuv_bin/lib
-// ./a.out
+// ./uv_kcp
+ 
+#include <uv.h>
+#include "ikcp.c"
 
 #include <thread>
 #include <cassert>
 #include <queue>
 #include <vector>
- 
-#include <uv.h>
-#include "ikcp.c"
+
+#include <time.h>
+#include <unistd.h>
+
+static inline unsigned long long nstime(void) {
+    struct timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    return ((unsigned long long)ts.tv_sec)*1e9 + ts.tv_nsec;
+}
+
+#define log(fmt, args...) 0
+// #define log(fmt, args...) log(fmt, ##args)
+
+static unsigned long long starttime;
 
 /////////////////////////////////////////////////////////////////////////////////
 
@@ -23,55 +37,57 @@ static ikcpcb *client_kcps[client_kcp_count];
 static char client_msg[] = "hello world";
 static int client_msg_len = sizeof(client_msg);
 static struct sockaddr_in client_addr;
-static int client_loop_count = 1;
+static int client_loop_count = 1; // 1000000
 static char client_buf[65536];
 
 // static uv_buf_t client_recv_buf = uv_buf_init(client_msg, client_kcp_count);
 
 
 static void client_alloc_cb(uv_handle_t* handle,
-                     size_t suggested_size,
-                     uv_buf_t* buf) {
+                            size_t suggested_size,
+                            uv_buf_t* buf) {
     assert(suggested_size <= sizeof(client_buf));
     buf->base = client_buf;
     buf->len = sizeof(client_buf);
 }
 
 static void client_recv_cb(uv_udp_t* handle,
-                       ssize_t nread,
-                       const uv_buf_t* rcvbuf,
-                       const struct sockaddr* addr,
-                       unsigned flags) {
+                           ssize_t nread,
+                           const uv_buf_t* rcvbuf,
+                           const struct sockaddr* addr,
+                           unsigned flags) {
     if (nread < IKCP_OVERHEAD) {
-        // printf("client_recv_cb"" rcvbuf->len < IKCP_OVERHEAD: %lu\n", rcvbuf->len);
+        // log("client_recv_cb"" rcvbuf->len < IKCP_OVERHEAD: %lu\n", rcvbuf->len);
         return;
     }
     IUINT32 conv;
     ikcp_decode32u(rcvbuf->base, &conv);
-    printf("client_recv_cb conv:%d, len:%ld\n", conv, nread);
+    log("client_recv_cb conv:%d, len:%ld\n", conv, nread);
     ikcpcb *kcp = client_kcps[conv];
     ikcp_input(kcp, rcvbuf->base, nread);
     int len = ikcp_recv(kcp, client_buf, sizeof(client_msg));
     if (len > 0) {
-        printf("client_recv_cb success %d\n", len);
+        log("client_recv_cb success %d\n", len);
         client_loop_count--;
         if (client_loop_count == 0) {
-            printf("success\n");
-            return;
+            unsigned long long stoptime = nstime();
+            printf("time diff: %llu ns\n", stoptime-starttime);
+            exit(0);
+            // return;
         };
-        printf("client ikcp_send len:%d\n", len);
+        log("client ikcp_send len:%d\n", len);
         assert(0 == ikcp_send(kcp, client_buf, len));
     }
 }
 
 static void client_udp_send_cb(uv_udp_send_t* req, int status) {
-    printf("client_udp_send_cb: %d\n", status);
+    log("client_udp_send_cb: %d\n", status);
     assert(0 == status);
     free(req);
 }
 
 static int client_kcp_output(const char *buf, int len, ikcpcb *kcp, void *user) {
-    printf("client_kcp_output: %d\n", len);
+    log("client_kcp_output: %d\n", len);
     uv_udp_send_t *req = (uv_udp_send_t *)malloc(sizeof(*req));
     uv_buf_t uv_buf = uv_buf_init((char *)buf, len);
     uv_udp_send(req, &client_udp, &uv_buf, 1, (const struct sockaddr*) &client_addr, client_udp_send_cb);
@@ -79,7 +95,7 @@ static int client_kcp_output(const char *buf, int len, ikcpcb *kcp, void *user) 
 }
 
 static void kcp_all_update(uv_timer_t *handle) {
-    // printf("kcp_all_update\n");
+    // log("kcp_all_update\n");
     uint64_t now = uv_now(&client_loop);
     // while(!client_queue.empty()) {
     //     ikcpcb *kcp = client_queue.top();
@@ -111,10 +127,14 @@ void client() {
         client_kcps[i] = ikcp_create(i, NULL);
         ikcp_nodelay(client_kcps[i], 1, 10, 2, 1);
         ikcp_setoutput(client_kcps[i], client_kcp_output);
-        printf("client ikcp_send len: %d\n", client_msg_len);
-        assert(0 == ikcp_send(client_kcps[i], client_msg, client_msg_len));
-
+        log("client ikcp_send len: %d\n", client_msg_len);
         // client_queue.push(client_kcps[i]);
+    }
+
+    starttime = nstime();
+    for (int i = 0; i < client_kcp_count; ++i)
+    {
+        assert(0 == ikcp_send(client_kcps[i], client_msg, client_msg_len));
     }
 
     uv_timer_t repeat;
@@ -138,15 +158,15 @@ static char server_buf[65536];
 // static int client_loop_count = 3;
 
 static void server_alloc_cb(uv_handle_t* handle,
-                     size_t suggested_size,
-                     uv_buf_t* buf) {
+                            size_t suggested_size,
+                            uv_buf_t* buf) {
     assert(suggested_size <= sizeof(server_buf));
     buf->base = server_buf;
     buf->len = sizeof(server_buf);
 }
 
 static int server_kcp_output(const char *buf, int len, ikcpcb *kcp, void *user) {
-    printf("server_kcp_output: %d\n", len);
+    log("server_kcp_output: %d\n", len);
     uv_udp_send_t *req = (uv_udp_send_t *)malloc(sizeof(*req)); // 在client_udp_send_cb中free
     uv_buf_t uv_buf = uv_buf_init((char *)buf, len);
     uv_udp_send(req, &server_udp, &uv_buf, 1, (const struct sockaddr*) user, client_udp_send_cb);
@@ -154,17 +174,17 @@ static int server_kcp_output(const char *buf, int len, ikcpcb *kcp, void *user) 
 }
 
 static void server_recv_cb(uv_udp_t* handle,
-                       ssize_t nread,
-                       const uv_buf_t* rcvbuf,
-                       const struct sockaddr* addr,
-                       unsigned flags) {
+                           ssize_t nread,
+                           const uv_buf_t* rcvbuf,
+                           const struct sockaddr* addr,
+                           unsigned flags) {
     if (nread < IKCP_OVERHEAD) {
-        // printf("server_recv_cb nread < IKCP_OVERHEAD: %d\n", nread);
+        // log("server_recv_cb nread < IKCP_OVERHEAD: %d\n", nread);
         return;
     }
     IUINT32 conv;
     ikcp_decode32u(rcvbuf->base, &conv);
-    printf("server_recv_cb conv:%d, len:%ld\n", conv, nread);
+    log("server_recv_cb conv:%d, len:%ld\n", conv, nread);
     if (server_kcps[conv] == NULL) {
         server_addrs[conv] = *addr;
         server_kcps[conv] = ikcp_create(conv, &server_addrs[conv]);
@@ -175,14 +195,14 @@ static void server_recv_cb(uv_udp_t* handle,
     ikcp_input(kcp, rcvbuf->base, nread);
     int len = ikcp_recv(kcp, server_buf, sizeof(client_msg));
     if (len > 0) {
-        printf("server ikcp_send: len: %d\n", len);
+        log("server ikcp_send: len: %d\n", len);
         int ret = ikcp_send(kcp, server_buf, len);
-        printf("server_recv_cb ikcp_send ret: %d\n", ret);
+        log("server_recv_cb ikcp_send ret: %d\n", ret);
     }
 }
 
 static void server_kcp_all_update(uv_timer_t *handle) {
-    // printf("kcp_all_update\n");
+    // log("kcp_all_update\n");
     uint64_t now = uv_now(&server_loop);
     // while(!client_queue.empty()) {
     //     ikcpcb *kcp = client_queue.top();
